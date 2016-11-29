f04102a2855762bd37cf249a03b73e2e6d359852
ItsDang <dangmatthewdang@gmail.com>
2016-11-29 01:05:26 -0500

    JS Update
    Updating JS to start reading from inputs.

--- SwiftApp/scripts.js
+++ SwiftApp/scripts.js
@@ -1,18 +1,30 @@
-alert("HELLO FROM TREEHOUSE");
-document.write("<h1>Welcome to JavaScript");
-alert("Thanks for Visiting!");
-var message = "Hi!, This site is going to allow to apply to some software engineering internships online without having to fill them all in!";
-alert(message);
-var name = prompt("Name");
-alert(name);
+// var inputs = document.getElementsByTagName("input");
+var requiredFieldSet = document.getElementById("required");
+var requiredInputs = requiredFieldSet.getElementsByTagName("input");
 
-var firstName;
-var lastName;
-var email;
-var phone;
-var resume;
-var coverletter;
-var university;
-var degree;
-var sponsorship;
-var linkedIn;
+var additionalFieldSet = document.getElementById("additional");
+// var additionalInputs = additionalFieldSet.getElementsByTagName("input");
+
+var companiesFieldSet = document.getElementById("companies");
+// var companiesInputs = companiesFieldSet.getElementsByTagName("input");
+
+var submitButton = document.getElementById("SubmitButton");
+
+
+
+//When Submit Pressed
+var submit = function() {
+	console.log("Submitting data.");
+	//Read in Required Fields
+	var fnamedata = requiredInputs[0].attributes;
+	// for(var i = 0; i < requiredInputs.length; i++) {
+	// 	console.log(i);
+	// }
+
+	//Read in Files
+
+	//Read in what companies are selected
+}
+
+//Set Button Click to submit function
+submitButton.onclick = submit;
