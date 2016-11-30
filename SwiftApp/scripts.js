745e4a441b47e7de0218319438088593454e0f81
ItsDang <dangmatthewdang@gmail.com>
2016-11-30 00:23:39 -0500

    JS and index update
    Updated the front end. and cleaned up the JS.

--- SwiftApp/scripts.js
+++ SwiftApp/scripts.js
@@ -20,23 +20,34 @@ var submit = function() {
 	console.log(emaildata);
 	var phonedata = requiredInputs[3].value;
 	console.log(phonedata);
+	//Is there a better way to check if fields/inputs are empty? also need to add check for US
 	if (fnamedata==""||lnamedata==""||emaildata==""||phonedata=="") {
 		console.log("A required field is empty!");
 		alert("A required field is empty!");
 	}
+	else {
+			//Read in Files
 
-	//Read in Files
-
-	//Read in what companies are selected
-	// console.log(companiesInputs);
-	// window.open();
-	for (var i = companiesInputs.length - 1; i >= 0; i--) {
-		if(companiesInputs[i].checked) {
-			console.log(companiesInputs[i].value);
-			window.open(companiesInputs[i].value);
+		//Read in what companies are selected
+		// console.log(companiesInputs);
+		// window.open();
+		for (var i = companiesInputs.length - 1; i >= 0; i--) {
+			if(companiesInputs[i].checked) {
+				console.log(companiesInputs[i].value);
+				var windowObjectReference = window.open(companiesInputs[i].value);
+				// windowObjectReference.focus();
+				// var fnamefield = windowObjectReference.document.getElementById("first_name");
+				
+				console.log(windowObjectReference);
+				console.log(windowObjectReference.document);
+				// console.log(fnamefield);
+				// windowObjectReference.document.open();
+				// windowObjectReference.document.body.innerHTML = "HETML";
+				// windowObjectReference.document.close();
+			}
 		}
+		// window.open();
 	}
-	// window.open();
 }
 
 //Set Button Click to submit function
